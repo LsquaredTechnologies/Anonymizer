93fa5983e6db84a6e7982c0727465e45404518fe
Lionel Lalande <lionel.lalande+github@lsquared.fr>
2026-06-22 13:58:35 +0200

    feat: create aot executable
    - No runtime required
    - Smaller file

--- src/Cli/Lifetime/Metadata.cs
+++ src/Cli/Lifetime/Metadata.cs
@@ -1,5 +1,6 @@
 using System.Runtime.Versioning;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Anonymizer.Cli.Lifetime;
 
@@ -13,13 +14,28 @@ internal sealed record class Metadata(
     public static async Task<Metadata> Load()
     {
         using var stream = File.OpenRead(Application.Metadata.Path);
-        var metadata = await JsonSerializer.DeserializeAsync<Metadata>(stream);
+        var metadata = await JsonSerializer.DeserializeAsync<Metadata>(stream, MetadataJsonContext.Default.Metadata);
         return metadata ?? new(null, null, string.Empty);
     }
 
     public async Task Save()
     {
         using var stream = File.Open(Application.Metadata.Path, FileMode.Create, FileAccess.Write);
-        await JsonSerializer.SerializeAsync(stream, this, Defaults.JsonOptions);
+        await JsonSerializer.SerializeAsync(stream, this, MetadataJsonContext.Default.Metadata);
     }
+}
+
+[JsonSerializable(typeof(Metadata))]
+[JsonSourceGenerationOptions(Converters = new[] { typeof(VersionJsonConverter) })]
+internal partial class MetadataJsonContext : JsonSerializerContext
+{
+}
+
+internal sealed class VersionJsonConverter : JsonConverter<Version>
+{
+    public override Version? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
+        Version.TryParse(reader.GetString(), out var v) ? v : null;
+
+    public override void Write(Utf8JsonWriter writer, Version value, JsonSerializerOptions options) =>
+        writer.WriteStringValue(value.ToString());
 }
