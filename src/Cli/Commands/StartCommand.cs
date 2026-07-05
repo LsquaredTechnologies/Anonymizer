10553adc7b964b39cd2e9e7f04bd0216a729195b
Lionel Lalande <lionel.lalande+github@lsquared.fr>
2026-07-05 14:57:36 +0200

    chore(cli): cleanup commands and config serialization

--- src/Cli/Commands/StartCommand.cs
+++ src/Cli/Commands/StartCommand.cs
@@ -4,7 +4,6 @@ using Anonymizer.Cli.Lifetime;
 
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.VisualBasic;
 
 namespace Anonymizer.Cli.Commands;
 
